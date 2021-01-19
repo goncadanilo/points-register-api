2879bdb30c19d68d65d0bf7f2117dac8a943d3e3
Danilo Gonçalves <gonca.danilo@gmail.com>
2021-01-19 15:36:26 -0300

    :sparkles: Create database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot()],
 })
 export class AppModule {}
