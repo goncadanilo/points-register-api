b049a7ae2849348e0469093c262d3cfe62ce5b40
Danilo Gonçalves <gonca.danilo@gmail.com>
2021-01-20 20:41:35 -0300

    :recycle: Typing the validate user method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,12 +2,13 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { compareSync } from 'bcrypt';
 import { UsersService } from 'src/users/users.service';
 import { AuthInput } from './dtos/auth.input';
+import { AuthType } from './dtos/auth.type';
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
 
-  async validateUser(data: AuthInput) {
+  async validateUser(data: AuthInput): Promise<AuthType> {
     const user = await this.usersService.findUserByEmail(data.email);
 
     const validPasssword = compareSync(data.password, user.password);
