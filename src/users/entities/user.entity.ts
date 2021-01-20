457cb11402c4fb6eeb278ad26a7b9376c29af650
Danilo Gonçalves <gonca.danilo@gmail.com>
2021-01-20 12:07:24 -0300

    :sparkles: Create user entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,27 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ name: 'role_id' })
+  roleId: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+}
