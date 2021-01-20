e194c56e02e4bd2d067e6c54dd65e0fe210c8458
Danilo Gonçalves <gonca.danilo@gmail.com>
2021-01-20 18:04:03 -0300

    :sparkles: Create validate user method

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { UsersService } from 'users/users.service';
 import { AuthService } from './auth.service';
 
 @Module({
-  providers: [AuthService],
+  providers: [AuthService, UsersService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { compareSync } from 'bcrypt';
+import { UsersService } from '../users/users.service';
+import { AuthInput } from './dtos/auth.input';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async validateUser(data: AuthInput) {
+    const user = await this.usersService.findUserByEmail(data.email);
+
+    const validPasssword = compareSync(data.password, user.password);
+
+    if (!validPasssword) {
+      throw new UnauthorizedException('Incorrect password');
+    }
+
+    return {
+      user,
+      token: 'token',
+    };
+  }
+}
