60474b706047c2208fd2c7a1b0c73220243cf06a
Danilo Gonçalves <gonca.danilo@gmail.com>
2021-01-20 20:44:05 -0300

    :sparkles: Create auth resolver

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
+import { AuthResolver } from './auth.resolver';
 
 @Module({
-  providers: [AuthService, UsersService],
+  providers: [AuthService, UsersService, AuthResolver],
 })
 export class AuthModule {}
