af5832a68b65e0307aa03056f7e610144ef6947c
Danilo Gonçalves <gonca.danilo@gmail.com>
2021-01-20 09:19:51 -0300

    :wrench: Change ORM settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { resolve } from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_NAME,
+      entities: [resolve(__dirname, '**', 'entities', '*.entity.{ts,js}')],
+      synchronize: process.env.POSTGRES_SYNC === 'true',
+    }),
+  ],
 })
 export class AppModule {}
