042fc3ff25e256ac2a5c27d5ca7ce415e00f9315
Danilo Gonçalves <gonca.danilo@gmail.com>
2021-01-20 14:02:49 -0300

    :sparkles: Ensure that be able to find a user by email

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repository: Repository<User>) {}
+
+  async findUserByEmail(email: string): Promise<User> {
+    return await this.repository.findOne({ email });
+  }
+}
