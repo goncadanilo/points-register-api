861e82c9e8e7b9bf477398f5a1f1d65de6ab4cc9
Danilo Gonçalves <gonca.danilo@gmail.com>
2021-01-20 18:06:26 -0300

    :white_check_mark: Ensure that the user is validated

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { hashSync } from 'bcrypt';
+import { UsersService } from 'users/users.service';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let usersService;
 
-  beforeEach(async () => {
+  const usersServiceFactory = {
+    findUserByEmail: jest.fn(),
+  };
+
+  const mockData = {
+    email: 'any@brainny.cc',
+    password: 'any',
+  };
+
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: UsersService, useFactory: () => usersServiceFactory },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    usersService = module.get<UsersService>(UsersService);
+  });
+
+  beforeEach(() => {
+    usersService.findUserByEmail.mockReset();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('when validate user', () => {
+    it('should validate user', async () => {
+      const password = hashSync(mockData.password, 10);
+      usersService.findUserByEmail.mockReturnValue({ ...mockData, password });
+
+      const result = await service.validateUser(mockData);
+
+      expect(result).toHaveProperty('token');
+      expect(usersService.findUserByEmail).toBeCalledWith(mockData.email);
+      expect(usersService.findUserByEmail).toBeCalledTimes(1);
+    });
   });
 });
