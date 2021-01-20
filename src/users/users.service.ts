af2d2434d1f7e3d468431c1b4807a35d2c9081f3
Danilo Gonçalves <gonca.danilo@gmail.com>
2021-01-20 14:29:15 -0300

    :sparkles: Ensure that throw error if user not found

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
@@ -8,6 +8,12 @@ export class UsersService {
   constructor(@InjectRepository(User) private repository: Repository<User>) {}
 
   async findUserByEmail(email: string): Promise<User> {
-    return await this.repository.findOne({ email });
+    const user = await this.repository.findOne({ email });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
   }
 }
