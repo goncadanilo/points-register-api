f9471a92f0731f7015eef7499c89b6bac18e8714
Danilo Gonçalves <gonca.danilo@gmail.com>
2021-01-21 02:24:15 -0300

    :sparkles: Create users resolver

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
+import { UsersResolver } from './users.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService],
+  providers: [UsersService, UsersResolver],
 })
 export class UsersModule {}
