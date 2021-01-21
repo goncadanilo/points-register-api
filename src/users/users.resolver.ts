d11c521845efcb45aea79e23a102012cca1eaba2
Danilo Gonçalves <gonca.danilo@gmail.com>
2021-01-21 02:40:28 -0300

    :sparkles: Ensure the return of the created user

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,14 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { CreateUserInput } from './dtos/create-user.input';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
 
-@Resolver()
-export class UsersResolver {}
+@Resolver('User')
+export class UsersResolver {
+  constructor(private readonly userService: UsersService) {}
+
+  @Mutation(() => User)
+  async createUser(@Args('data') data: CreateUserInput): Promise<User> {
+    return await this.userService.createUser(data);
+  }
+}
