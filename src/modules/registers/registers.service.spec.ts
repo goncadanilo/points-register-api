349efd01fb5f5136e4b0b09313483c2f8b3207d6
Danilo Gonçalves <gonca.danilo@gmail.com>
2021-01-21 20:22:47 -0300

    :white_check_mark: Ensure that the register is created

--- src/modules/registers/registers.service.spec.ts
+++ src/modules/registers/registers.service.spec.ts
@@ -1,18 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Register } from './entities/register.entity';
 import { RegistersService } from './registers.service';
 
 describe('RegistersService', () => {
   let service: RegistersService;
 
-  beforeEach(async () => {
+  const repositoryMock = {
+    create: jest.fn(),
+    save: jest.fn(),
+  };
+
+  const mockData = {
+    userId: 'any_id',
+    timeRegistered: new Date(),
+  };
+
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RegistersService],
+      providers: [
+        RegistersService,
+        { provide: getRepositoryToken(Register), useValue: repositoryMock },
+      ],
     }).compile();
 
     service = module.get<RegistersService>(RegistersService);
   });
 
+  beforeEach(() => {
+    repositoryMock.create.mockReset();
+    repositoryMock.save.mockReset();
+  });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('when create a register', () => {
+    it('should be create a employee', async () => {
+      repositoryMock.create.mockReturnValue(mockData);
+      repositoryMock.save.mockReturnValue({ ...mockData, id: 'any_id' });
+
+      const register = await service.createRegister(mockData);
+
+      expect(register).toHaveProperty('id');
+      expect(register).toMatchObject(mockData);
+      expect(repositoryMock.create).toBeCalledWith(mockData);
+      expect(repositoryMock.create).toBeCalledTimes(1);
+      expect(repositoryMock.save).toBeCalledWith(mockData);
+      expect(repositoryMock.save).toBeCalledTimes(1);
+    });
   });
 });
