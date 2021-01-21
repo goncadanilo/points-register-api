d2033ee8974ffac3c8987f53374050d66a3281ef
Danilo Gonçalves <gonca.danilo@gmail.com>
2021-01-20 21:41:16 -0300

    :sparkles: Create login method

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,4 +1,19 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { AuthService } from './auth.service';
+import { AuthInput } from './dtos/auth.input';
+import { AuthType } from './dtos/auth.type';
 
 @Resolver()
-export class AuthResolver {}
+export class AuthResolver {
+  constructor(private authService: AuthService) {}
+
+  @Mutation(() => AuthType)
+  public async login(@Args('data') data: AuthInput): Promise<AuthType> {
+    const response = await this.authService.validateUser(data);
+
+    return {
+      user: response.user,
+      token: response.token,
+    };
+  }
+}
